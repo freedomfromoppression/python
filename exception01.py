808c8656a22dfa841e6f24e5558fbf60e787d7d8
freedomfromoppression <156163948+freedomfromoppression@users.noreply.github.com>
2024-02-21 17:32:01 +0900

    Add files via upload

---
+++ exception01.py
@@ -0,0 +1,20 @@
+#try...except
+def calc(values):
+    sum = 0
+    try:
+        sum = values[0] + values[1] + values[2]
+    except IndexError as arr:
+        print("index error") #index 오류만
+    except Exception as e:
+        print(str(e)) #모든 에러상황
+    else:
+        print("에러 없음") #에러가 없을떄만 실행됨
+    finally:
+        print(sum) #에러가 있던 없던 실행됨.
+#calc([1, 2])
+calc([True, True, True])
+
+try:
+    print("^^" % 4)
+except Exception as e:
+    print(str(e))
