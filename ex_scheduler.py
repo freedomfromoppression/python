046aaaca603a5d73955d6304bacea24ee1423cc5
freedomfromoppression <156163948+freedomfromoppression@users.noreply.github.com>
2024-02-26 14:05:27 +0900

    Add files via upload

---
+++ ex_scheduler.py
@@ -0,0 +1,20 @@
+#주기적으로 실행할 수 있게 해주는 라이브러리
+# pip install apscheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+import pytz
+import datetime
+seoul = pytz.timezone('Asia/seoul')
+#interval  :주기적으로 실행
+#cron      :원하는 시간, 다양한 시간에 실행
+def test_interval():
+    print("interval")
+    print(datetime.datetime.now())
+def test_cron():
+    print("cron")
+    print(datetime.datetime.now())
+sched = BlockingScheduler()
+# job 등록 1초마다 함수 호출
+sched.add_job(test_interval, 'interval', seconds=1, timezone=seoul)
+#cron 원하는 주기, 시간설정 mon-fri 매주 월요일~금요일, 15:35 호출
+sched.add_job(test_cron, 'cron', day_of_week='mon-fri', hour='15', minute='37',timezone=seoul )
+sched.start()
